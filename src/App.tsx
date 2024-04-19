ebaaea66dd4eee03be56ef61d4f14e0167eabf63
sakinurs <sakinurs@mail.ru>
2024-04-20 00:43:57 +0600

    integrate redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import viteLogo from '/vite.svg';
 
 function App() {
     const [count, setCount] = useState(0);
-
     return (
         <>
             <div>
@@ -18,7 +17,7 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
